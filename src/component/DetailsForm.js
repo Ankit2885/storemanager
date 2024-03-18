229679c6758fb0c40f681ac40eaee66d0a60a70f
vega6Ankit <ankit@vega6.com>
2024-03-18 17:01:04 +0530

    update changes

--- src/component/DetailsForm.js
+++ src/component/DetailsForm.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 
 const DetailsForm = () => {
+
+    const [state, setState] = useState({
+        name: "",
+        phone: ""
+    });
     const [radio, setRadio] = useState("/")
+    const dispatch = useDispatch()
 
     let history = useHistory();
 
-    const handelClick = (e) => {
+    const handleChange = (e) => {
+        const { name, phone } = e.target
+        setState({
+            ...state,
+            name: name,
+            phone: phone,
+        })
+    }
+
+    const handleSubmit = (e) => {
         e.preventDefault();
         history.push(radio);
+        let data = { ...state }
+        // pending action 
+        // dispatch(onCreateCustomer(data))
     }
     const getValue = (e) => {
         let radioValue = "/" + e.target.value;
@@ -18,14 +37,14 @@ const DetailsForm = () => {
     return (
         <div className="container my-3">
             <h1>Store Manager</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" name='name' aria-describedby="emailHelp" />
+                    <input type="text" className="form-control" id="name" name='name' aria-describedby="emailHelp" value={state.name} onChange={handleChange} required />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="phone" className="form-label">Phone Number</label>
-                    <input type="number" className="form-control" id="phone" name='phone' />
+                    <input type="number" className="form-control" id="phone" name='phone' value={state.phone} onChange={handleChange} required />
                 </div>
                 <div className="form-check">
                     <input className="form-check-input" type="radio" name="itemRadios" id="liquiditem" value="liquiditem" onClick={getValue} />
@@ -45,7 +64,7 @@ const DetailsForm = () => {
                         Both Item
                     </label>
                 </div>
-                <button className="btn btn-primary my-3" onClick={handelClick}>click</button>
+                <button type='submit' className="btn btn-primary my-3">click</button>
             </form>
         </div>
     )
